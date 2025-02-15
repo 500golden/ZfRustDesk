06bc7a9b37753043a59f3799bbed031f8d66c2a4
500golden <goldx500@163.com>
2025-02-15 16:55:29 +0800

    modify_RS_PUB_KEY

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -97,8 +97,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["122.224.218.37"];
+pub const RS_PUB_KEY: &str = "h3jrVQB7B3yEU+pptOpBffYlf13ScUkibxUKW43EZyQ=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
